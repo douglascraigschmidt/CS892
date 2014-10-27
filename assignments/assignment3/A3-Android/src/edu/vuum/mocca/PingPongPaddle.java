dafeb31e2f39a0ed9d7302c8df0314fdb684f35b
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-10-27 16:51:02 -0500

    Arrgh

--- assignments/assignment3/A3-Android/src/edu/vuum/mocca/PingPongPaddle.java
+++ assignments/assignment3/A3-Android/src/edu/vuum/mocca/PingPongPaddle.java
@@ -13,24 +13,18 @@ import java.util.concurrent.locks.ReentrantLock;
 public class PingPongPaddle {
     /** Condition used to wait for the Pingpong ball. */
     // TODO - You fill in here.
-    Condition mBall;
 
     /** Lock used along with the Condition. */
     // TODO - You fill in here.
-    Lock mLock;
 
     /** Do we have the ball or not. */
     // TODO - You fill in here.
-    boolean mHaveBall;
 
     /**
      * Constructor initializes data members.
      */
     public PingPongPaddle(boolean haveBall) {
         // TODO - You fill in here.
-        mLock = new ReentrantLock();
-        mBall = mLock.newCondition();
-        mHaveBall = haveBall;
     }
 
     /**
@@ -38,13 +32,6 @@ public class PingPongPaddle {
      */
     public void returnBall() {
         // TODO - You fill in here.
-        mLock.lock();
-        try {
-            mHaveBall = true;
-            mBall.signal();
-        } finally {
-            mLock.unlock();
-        }
     }
 
     /**
@@ -52,15 +39,6 @@ public class PingPongPaddle {
      */
     public void awaitBall() {
         // TODO - You fill in here.
-        mLock.lock();
-        try {
-            // Wait until we've been hit the ball.
-            while (mHaveBall == false)
-                mBall.awaitUninterruptibly();
-            mHaveBall = false;
-        } finally {
-            mLock.unlock();
-        }
     }
 }
 
