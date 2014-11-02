f75964e9e0de2b4109d35b1efbebbf66bec19221
Mitchell Gordon <mitchell.gordon95@gmail.com>
2014-11-02 16:09:49 -0600

    A couple fixes

--- assignments/assignment3/A3-Android-Test/src/edu/vuum/mocca/test/TestOptions.java
+++ assignments/assignment3/A3-Android-Test/src/edu/vuum/mocca/test/TestOptions.java
@@ -17,7 +17,7 @@ public class TestOptions {
     /**
      * Time we should wait for things to run.
      */
-    static final long ACCEPTABLE_RUNTIME_LENGTH = 2000;
+    static final long ACCEPTABLE_RUNTIME_LENGTH = 15000;
 
     /**
      * Time we should wait for things to download.
@@ -33,6 +33,6 @@ public class TestOptions {
     static final String PONG_ALL_FIRST_WINDOWS = "Ready...Set...Go!\r\n_pong (1)\r\n_pong (2)\r\n_pong (3)\r\n_pong (4)\r\n_pong (5)\r\n_pong (6)\r\n_pong (7)\r\n_pong (8)\r\n_pong (9)\r\n_pong (10)\r\nping  (1)\r\nping  (2)\r\nping  (3)\r\nping  (4)\r\nping  (5)\r\nping  (6)\r\nping  (7)\r\nping  (8)\r\nping  (9)\r\nping  (10)\r\nDone!";
     static final String PONG_ALL_FIRST_UNIX = "Ready...Set...Go!\n_pong (1)\n_pong (2)\n_pong (3)\n_pong (4)\n_pong (5)\n_pong (6)\n_pong (7)\n_pong (8)\n_pong (9)\n_pong (10)\nping  (1)\nping  (2)\nping  (3)\nping  (4)\nping  (5)\nping  (6)\nping  (7)\nping  (8)\nping  (9)\nping  (10)\nDone!";
     static final String JAVA_CONSOLE_WINDOWS = "Ready...Set...Go!\r\nping  (1)\r\n_pong (1)\r\nping  (2)\r\n_pong (2)\r\nping  (3)\r\n_pong (3)\r\nping  (4)\r\n_pong (4)\r\nping  (5)\r\n_pong (5)\r\nping  (6)\r\n_pong (6)\r\nping  (7)\r\n_pong (7)\r\nping  (8)\r\n_pong (8)\r\nping  (9)\r\n_pong (9)\r\nping  (10)\r\n_pong (10)\r\nDone!";
-    static final String ANDROID_TEXTVIEW = "Ready...Set...Go!\nping    (1)\n_pong (1)\nping    (2)\n_pong (2)\nping    (3)\n_pong (3)\nping    (4)\n_pong (4)\nping    (5)\n_pong (5)\nping    (6)\n_pong (6)\nping    (7)\n_pong (7)\nping    (8)\n_pong (8)\nping    (9)\n_pong (9)\nping    (10)\n_pong (10)\nDone!\n";
+    static final String ANDROID_TEXTVIEW = "Ready...Set...Go!\nping(1)\npong(1)\nping(2)\npong(2)\nping(3)\npong(3)\nping(4)\npong(4)\nping(5)\npong(5)\nping(6)\npong(6)\nping(7)\npong(7)\nping(8)\npong(8)\nping(9)\npong(9)\nping(10)\npong(10)\nDone!\n";
 
 }
