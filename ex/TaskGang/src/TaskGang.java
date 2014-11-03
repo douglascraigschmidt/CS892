db299107b7c3a8b864949dc15ee9b2a802e938b7
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2014-11-03 13:56:55 -0600

    Updates

--- ex/TaskGang/src/TaskGang.java
+++ ex/TaskGang/src/TaskGang.java
@@ -1,13 +1,13 @@
 import java.util.List;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @class TaskGang
  *
  * @brief Defines a framework for spawning and running a "gang" of
- *        Threads that concurrently process input from a generic
- *        List of elements E for one or more cycles.
+ *        tasks that concurrently process input from a generic List of
+ *        elements E for one or more cycles.
  */
 public abstract class TaskGang<E> implements Runnable {
     /**
@@ -17,30 +17,16 @@ public abstract class TaskGang<E> implements Runnable {
     private volatile List<E> mInput = null;
 
     /**
+     * Executes submitted Runnable tasks in a Thread pool.
+     */
+    private Executor mExecutor = null;
+        
+    /**
      * Keeps track of which cycle is currently active.
      */
     private final AtomicLong mCurrentCycle = 
         new AtomicLong(0);
     
-    /**
-     * Executes submitted Runnable tasks in a Thread pool.
-     */
-    private ExecutorService mExecutorService = null;
-        
-    /**
-     * Increment to the next cycle.
-     */
-    protected long incrementCycle() {
-        return mCurrentCycle.incrementAndGet();
-    }
-
-    /**
-     * Return the current cycle.
-     */
-    protected long currentCycle() {
-        return mCurrentCycle.get();
-    }
-
     /**
      * Get the List to use as input.
      */
@@ -56,31 +42,46 @@ public abstract class TaskGang<E> implements Runnable {
     }
 
     /**
-     * Set the ExecutorService to use to submit/run tasks.
+     * Set the Executor to use to submit/run tasks.
      */
-    protected void setExecutorService(ExecutorService executorService) {
-        mExecutorService = executorService;
+    protected void setExecutor(Executor executor) {
+        mExecutor = executor;
     }
 
     /**
-     * Get the ExecutorService to use to submit/run tasks.
+     * Get the Executor to use to submit/run tasks.
      */
-    protected ExecutorService getExecutorService() {
-        return mExecutorService;
+    protected Executor getExecutor() {
+        return mExecutor;
+    }
+
+    /**
+     * Increment to the next cycle.
+     */
+    protected long incrementCycle() {
+        return mCurrentCycle.incrementAndGet();
+    }
+
+    /**
+     * Return the current cycle.
+     */
+    protected long currentCycle() {
+        return mCurrentCycle.get();
     }
 
     /**
      * Factory method that makes the next List of input to be
-     * processed concurrently by the gang of Threads.
+     * processed concurrently by the gang of Tasks.
      */
     protected abstract List<E> getNextInput();
 
     /**
      * Hook method called back by initiateTaskGang() to enable
-     * subclasses to perform custom initializations before the Threads
+     * subclasses to perform custom initializations before the tasks
      * in the gang are spawned.
      */
     protected void initiateHook(int inputSize) {
+        // No-op by default.
     }
 
     /**
@@ -100,31 +101,31 @@ public abstract class TaskGang<E> implements Runnable {
 
     /**
      * Hook method that can be used as an exit barrier to wait for the
-     * gang of Threads to exit.
+     * gang of tasks to exit.
      */
     protected abstract void awaitTasksDone();
 
     /**
-     * Hook method called when a worker Thread is done.  Can be used
-     * in conjunction with a one-shop or cyclic barrier to wait for
-     * all the other Threads to complete their current cycle.  Is
-     * passed the index of the work that's done.  Returns true if the
-     * wait was successfuly or throws the IndexOutOfBoundsException if
-     * the item has been removed.
+     * Hook method called when a task is done.  Can be used in
+     * conjunction with a one-shop or cyclic barrier to wait for all
+     * the other tasks to complete their current cycle.  It's passed
+     * the index of the work that's done.  Returns true if the wait
+     * was successfuly or throws the IndexOutOfBoundsException if the
+     * item has been removed.
      */
     protected void taskDone(int index) throws IndexOutOfBoundsException {
         // No-op.
     }
     
     /**
-     * Hook method that performs work a background Thread.  Returns
-     * true if all goes well, else false (which will stop the
-     * background Thread from continuing to run).
+     * Hook method that performs work a background task.  Returns true
+     * if all goes well, else false (which will stop the background
+     * task from continuing to run).
      */
     protected abstract boolean processInput(E inputData);
 
     /**
-     * Template method that creates/runs all the Threads in the gang.  
+     * Template method that creates/runs all the tasks in the gang.
      */
     @Override
     public void run() {
@@ -132,23 +133,23 @@ public abstract class TaskGang<E> implements Runnable {
         // process.
         setInput (getNextInput());
 
-        // Invoke method to initialize the gang of Threads.
+        // Invoke method to initialize the gang of tasks.
         initiateTaskGang(getInput().size());
 
-        // Invoke hook method to wait for all the Threads to exit.
+        // Invoke hook method to wait for all the tasks to exit.
         awaitTasksDone();
     }
 
     /**
      * Factory method that creates a Runnable task that will process
      * one node of the input List (at location @code index) in a
-     * background Thread provided by the ExecutorService.
+     * background task provided by the Executor.
      */
     protected Runnable makeTask(final int index) {
         return new Runnable() {
 
-            // This method runs in background Thread provided by the
-            // ExecutorService.
+            // This method runs in background task provided by the
+            // Executor.
             public void run() {
                 try {
                     // Get the input data element associated with
@@ -157,7 +158,7 @@ public abstract class TaskGang<E> implements Runnable {
 
                     // Process input data element.
                     if (processInput(element))
-                        // Success indicates the worker Thread is done
+                        // Success indicates the worker task is done
                         // with this cycle.
                         taskDone(index);
                     else
