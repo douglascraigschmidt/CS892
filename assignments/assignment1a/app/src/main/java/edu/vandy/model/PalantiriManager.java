650c075302d51dab1463216e2909336adb05abf6
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-01-18 14:26:36 -0600

    Update

--- assignments/assignment1a/app/src/main/java/edu/vandy/model/PalantiriManager.java
+++ assignments/assignment1a/app/src/main/java/edu/vandy/model/PalantiriManager.java
@@ -1,13 +1,15 @@
 package edu.vandy.model;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+
+import edu.vandy.utils.UiUtils;
 
 /**
  * Defines a mechanism that mediates thread-safe access to a fixed
- * number of available Palantiri using a Java ConcurrentHashMap.  This
- * implementation is intentionally very simple and is not meant to be
- * used in production code.
+ * number of available Palantiri using a Java list that's synchronized
+ * by a volatile "spin lock".  This implementation is intentionally
+ * very simple and is not meant to be used in production code.
  */
 public class PalantiriManager {
     /**
@@ -17,29 +19,59 @@ public class PalantiriManager {
         PalantiriManager.class.getSimpleName();
 
     /**
-     * A ConcurrentHashMap that associates the @a Palantiri key to
-     * the @a boolean values that keep track of whether the key is
-     * available.
+     * Keeps track of whether a Palantir is available.
      */
-    protected final ConcurrentHashMap<Palantir, Boolean> mPalantiriMap;
+    private class PalantirEntry {
+        /**
+         * The Palantir itself.
+         */
+        Palantir mPalantir;
+
+        /**
+         * A flag that keeps track of whether the Palantir is
+         * available.
+         */
+        boolean mAvailable;
+
+        /**
+         * Constructor initializes the field.
+         */
+        public PalantirEntry(Palantir palantir, boolean available) {
+            mPalantir = palantir;
+            mAvailable = available;
+        }
+    }
+
+    /**
+     * A volatile flag that's used as a "spin lock" to ensure that
+     * threads serialize on a critical section.
+     */
+    volatile boolean mLocked;
+
+    /**
+     * A list that associates the @a Palantiri key to the @a boolean
+     * values that keep track of whether the key is available.
+     */
+    protected final List<PalantirEntry> mPalantiriList;
 
     /**
      * Constructor creates a PalantiriManager for the List of @a
      * palantiri passed as a parameter and initializes the fields.
      */
     public PalantiriManager(List<Palantir> palantiri) {
-        // Create a new ConcurrentHashMap, iterate through the List of
-        // Palantiri and initialize each key in the HashMap with
-        // "true" to indicate it's available.
+        // Create a new List, iterate through the List of Palantiri
+        // and initialize each key in the HashList with "true" to
+        // indicate it's available.
 
-        // Create a new ConcurrentHashMap.
-        mPalantiriMap = new ConcurrentHashMap<>();
+        // Create a new List.
+        mPalantiriList = new ArrayList<>();
 
         // Iterate through the List of Palantiri and initialize each
-        // key in the mPalanatiriMap with "true" to indicate it's
+        // key in the mPalanatiriList with "true" to indicate it's
         // available.
         palantiri.forEach(palantir
-                          -> mPalantiriMap.put(palantir, true));
+                          -> mPalantiriList.add(new PalantirEntry(palantir,
+                                                                  true)));
     }
 
     /**
@@ -48,16 +80,29 @@ public class PalantiriManager {
      * palantir available.
      */
     public Palantir acquire() {
-        // Use ConcurrentHashMap.search() to ensure thread-safety.
-        return mPalantiriMap.search(1, (palantir, available) -> {
-                // If the palantir is available mark it as being
-                // not available and return the palantir.
-                if (available) {
-                    mPalantiriMap.put(palantir, false);
-                    return palantir;
-                } else
-                    return null;
-            });
+        Palantir palantir = null;
+
+        // Spin until we get the lock.
+        while (mLocked == true)
+            UiUtils.pauseThread(100);
+
+        // "Acquire" the lock.
+        mLocked = true;
+
+        // Find an available palantiri if one exists.
+        for (PalantirEntry pe : mPalantiriList) {
+            // If the palantir is available mark it as being not
+            // available and return the palantir.
+            if (pe.mAvailable == true) {
+                pe.mAvailable = false;
+                palantir = pe.mPalantir;
+                break;
+            }
+        }
+            
+        // "Release" the lock.
+        mLocked = false;
+        return palantir;
     }
 
     /**
@@ -65,15 +110,31 @@ public class PalantiriManager {
      * so that it's available for other Threads to use.
      */
     public void release(final Palantir palantir) {
-        // Put the "true" value back into HashMap for the palantir key
-        // in a thread-safe manner.
+        // Put the "true" value back into List for the palantir key in
+        // a thread-safe manner.
 
         // Do a simple sanity check!
-        if (palantir != null) 
-            // Put the "true" value back into HashMap for the palantir
-            // key, which also atomically returns the boolean
-            // associated with the palantir.
-            mPalantiriMap.put(palantir, true);
+        if (palantir != null) {
+            // Spin until we get the lock.
+            while (mLocked == true)
+                UiUtils.pauseThread(100);
+
+            // "Acquire" the lock.
+            mLocked = true;
+
+            // Iterate through all the entries in the list.
+            for (PalantirEntry pe : mPalantiriList) {
+                // Make the palantir available (i.e., set it to
+                // "true") when the palantir matches.
+                if (pe.mPalantir == palantir) {
+                    pe.mAvailable = true;
+                    break;
+                }
+            }
+            
+            // "Release" the lock.
+            mLocked = false;
+        }
     }
 
     /*
@@ -87,9 +148,27 @@ public class PalantiriManager {
     public int availablePermits() {
         // Returns a count of the number of palantiri whose
         // values are "true" (i.e., available for use).
-        return mPalantiriMap.reduceValuesToInt(1,
-                                               v -> v ? 1 : 0,
-                                               0,
-                                               (x, y) -> x + y);
+
+        // Keeps track of the palantiri that are available.
+        int availablePalantiri = 0;
+
+        // Spin until we get the lock.
+        while (mLocked == true)
+            UiUtils.pauseThread(100);
+
+        // "Acquire" the lock.
+        mLocked = true;
+
+        // Find an available palantiri if one exists.
+        for (PalantirEntry pe : mPalantiriList) {
+            // If the palantir is available then increment the count.
+            if (pe.mAvailable == true) 
+                ++availablePalantiri;
+        }
+
+        // "Release" the lock.
+        mLocked = false;
+
+        return availablePalantiri;
     }
 }
