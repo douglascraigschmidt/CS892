40cd6318c34b1bff229246b8ecbc4402789db0fb
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-03-27 14:20:34 -0500

    review

--- assignments/assignment4a/app/src/main/java/edu/vandy/presenter/BeingCallable.java
+++ assignments/assignment4a/app/src/main/java/edu/vandy/presenter/BeingCallable.java
@@ -8,6 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import edu.vandy.model.Palantir;
 import edu.vandy.model.PalantiriModel;
 import edu.vandy.utils.Options;
+import edu.vandy.utils.UiUtils;
 import edu.vandy.view.GazingSimulationActivity;
 
 /**
